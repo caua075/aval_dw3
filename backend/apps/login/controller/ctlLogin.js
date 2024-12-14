519c0b9aadb66d793da2fa7110fb44769858da0d
PabloGuedes <guedespablobacca@gmail.com>
2024-12-14 14:29:20 -0300

    Commit tela login, index e jogadores

--- backend/apps/login/controller/ctlLogin.js
+++ backend/apps/login/controller/ctlLogin.js
@@ -3,25 +3,18 @@ const bCrypt = require("bcryptjs");
 const mdlLogin = require("../model/mdlLogin");
 
 const Login = async (req, res, next) => {
-  console.log("Login request received:", req.body);
 
   // Buscar as credenciais no banco de dados
   const credencial = await mdlLogin.GetCredencial(req.body.username);
 
-  console.log("Credenciais recebidas:", credencial);
-
   // Verifica se as credenciais foram encontradas
   if (credencial.length == 0) {
-    console.log("Usuário não identificado", req.body.username);
     return res.status(403).json({ message: "Usuário não identificado!" });
   }
 
-  console.log("Senha fornecida:", req.body.password);
-  console.log("Senha armazenada:", credencial[0].password);
 
   // Verifique se a senha fornecida é igual à senha armazenada (usando bcrypt)
   if (bCrypt.compareSync(req.body.password, credencial[0].password)) {
-    console.log("Autenticação bem-sucedida");
 
     // Gera o token JWT
     const username = credencial[0].username;
@@ -29,10 +22,8 @@ const Login = async (req, res, next) => {
       expiresIn: 120 * 60, // Expira em 2 horas
     });
 
-    console.log("Login bem-sucedido para", username);
     return res.json({ auth: true, token: token });
   } else {
-    console.log("Senha inválida ou não encontrada");
     return res.status(403).json({ message: "Login inválido!" });
   }
 };
--- backend/apps/login/model/mdlLogin.js
+++ backend/apps/login/model/mdlLogin.js
@@ -1,13 +1,11 @@
 const pool = require('../../../database/databaseConfig');
 
 const GetCredencial = async (loginPar) => {
-  console.log("Buscando credenciais para:", loginPar);
   try {
     const result = await pool.query(
       "SELECT username, password FROM usuarios WHERE username = $1 AND deleted = false",
       [loginPar]
     );
-    console.log("Credenciais encontradas:", result.rows);
     return result.rows;
   } catch (error) {
     console.error('Erro ao acessar o banco de dados:', error);
