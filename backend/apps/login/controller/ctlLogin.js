9df0dece9156fb1b89066ba913c2acc7ed9b5cd4
PabloGuedes <guedespablobacca@gmail.com>
2024-12-14 14:22:15 -0300

    Commit login funcionando

--- backend/apps/login/controller/ctlLogin.js
+++ backend/apps/login/controller/ctlLogin.js
@@ -2,37 +2,42 @@ const jwt = require("jsonwebtoken");
 const bCrypt = require("bcryptjs");
 const mdlLogin = require("../model/mdlLogin");
 
-const Login = async (req, res, next) => { 
+const Login = async (req, res, next) => {
   console.log("Login request received:", req.body);
+
+  // Buscar as credenciais no banco de dados
   const credencial = await mdlLogin.GetCredencial(req.body.username);
-  
-  console.log("Credenciais recebidas:", credencial);    
-   
+
+  console.log("Credenciais recebidas:", credencial);
+
+  // Verifica se as credenciais foram encontradas
   if (credencial.length == 0) {
-    console.log("Usuário não identificado", req.body.UserName);
-    return res.status(403).json({ message: "Usuário não identificado!" });    
-  }  
+    console.log("Usuário não identificado", req.body.username);
+    return res.status(403).json({ message: "Usuário não identificado!" });
+  }
 
   console.log("Senha fornecida:", req.body.password);
   console.log("Senha armazenada:", credencial[0].password);
 
-  if (bCrypt.compareSync(req.body.Password, credencial[0].password)) {
-    console.log("Autenticação bem-sucedida"); 
+  // Verifique se a senha fornecida é igual à senha armazenada (usando bcrypt)
+  if (bCrypt.compareSync(req.body.password, credencial[0].password)) {
+    console.log("Autenticação bem-sucedida");
+
+    // Gera o token JWT
     const username = credencial[0].username;
     const token = jwt.sign({ username }, process.env.JWT_SECRET, {
-      expiresIn: 120 * 60, // Expira em 02 horas
+      expiresIn: 120 * 60, // Expira em 2 horas
     });
+
     console.log("Login bem-sucedido para", username);
     return res.json({ auth: true, token: token });
   } else {
     console.log("Senha inválida ou não encontrada");
-    res.status(403).json({ message: "Login inválido!" });
+    return res.status(403).json({ message: "Login inválido!" });
   }
-
-  console.log("Login inválido para", req.body.UserName);
-  res.status(403).json({ message: "Login inválido!" });
 };
 
+// Função para autenticar o JWT
 function AutenticaJWT(req, res, next) {
   const tokenHeader = req.headers["authorization"];
   if (!tokenHeader)
@@ -54,6 +59,7 @@ function AutenticaJWT(req, res, next) {
   });
 }
 
+// Função de logout
 const Logout = (req, res, next) => {
   res.json({ auth: false, token: null });
 };
